c57ef72111a065562b90221400c360849de5dfec
saymtfmtfmtf <mfenton@csumb.edu>
2015-04-08 23:51:05 +0000

    Added the index.php login screen

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title></title>
+        <link href="css/stylesheet.css" rel="stylesheet"/>
+    </head>
+    
+    <body>
+        <form action="adminLogin.php" method="post">
+            Username: <input type="text" name="username"/>
+            Password: <input type="text" name="password"/>
+            <input type="submit" value="Login"/>
+        </form>
+    </body>
+</html>
