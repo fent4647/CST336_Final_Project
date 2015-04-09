11d669ef43c2ebed19fbec71dbfffbb95230fa8a
saymtfmtfmtf <mfenton@csumb.edu>
2015-04-09 00:20:02 +0000

    Added Longin.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     </head>
     
     <body>
+        <p><?=if(isset($_GET['error']{ echo $_GET['error']; }))?></p>
+        
         <form action="login.php" method="post">
             Username: <input type="text" name="username"/>
             Password: <input type="text" name="password"/>
--- login.php
+++ login.php
@@ -1,4 +1,23 @@
 <?php
     require('includes/connect.php');
+    $dbConn = getConnection();
+
+    $username = $_POST['username'];
+    $password = sha1($_POST['password']);
+
+    $sql = "SELECT * FROM admin WHERE username = :username AND password = :password";
+    $stmt = $dbConn->prepare($sql);
+    $stmt->execute(array(
+            ":username"=>$username;
+            ":password"=>$password;
+    ));
+    $results = $stmt->fetch();    
     
+
+    if(empty($results)) {
+        header("Location: index.php?error=WRONG USERNAME/PASSWORD");
+    }else {
+        header("Location: mainhome.php");
+    }
+
 ?>
