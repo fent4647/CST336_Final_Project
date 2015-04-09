b912c61338ef34b6f86afda53c416af0d4580efe
galv5639 <cegalvan@csumb.edu>
2015-04-09 00:43:36 +0000

    fixed database connection

--- includes/connect.php
+++ includes/connect.php
@@ -5,7 +5,7 @@
         $user = "root";
         $pass = "s3cr3t";
         
-        $dbConn = new PDO("mysql:host:$host;dbname:$dbname;", $user, $pass);
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbname;", $user, $pass);
         $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
         return $dbConn;
