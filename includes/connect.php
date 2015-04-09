ef05b3552bfe9f3f39f7e377c7920d74a85794ec
galv5639 <cegalvan@csumb.edu>
2015-04-09 00:19:06 +0000

    added database location

--- includes/connect.php
+++ includes/connect.php
@@ -1,9 +1,9 @@
 <?php
     getConnection() {
-        $host = "";
-        $dbname = "";
-        $user = "";
-        $pass = "";
+        $host = "localhost";
+        $dbname = "airforce";
+        $user = "root";
+        $pass = "s3cr3t";
         
         $dbConn = new PDO("mysql:host:$host;dbname:$dbname;", $user, $pass);
         
