b4721cfb67bbb8c6dd7bb9dee49ed5e0b6057d5d
DWGarcia <dalgarcia@csumb.edu>
2015-04-29 16:39:17 +0000

    Added the Admin Table prototype for displaying the childen present and relevant information

--- admindash.php
+++ admindash.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require('includes/session.php');
+    //require('includes/session.php');
 ?>
 
 <!DOCTYPE HTML>
@@ -16,13 +16,79 @@
                 <nav>
                     <a href="">Main</a>
                     <a href="">Checked In</a>
+                    <a href="">Checkout</a>
                     <a href="logout.php">Logout</a>
                 </nav>
+                <br />
+                <table border="7"><!----------- Start Inserted table ----------->
+
+    <?php
+    //Row <->
+    //Col ^|V
+    /*This table Displays each Child's Informaton including: isPresent, child's Name,
+     *      Allergies, Emergency contact's Name, and Emergency Contact Phone Number.
+     *      This also table is formated as the following:
+     *          First for loop displays the table header.
+     *          Second for loop displays each of the child's information Row by Row.
+     */
+            $Header = ["Present", "Child Name", "Allergies", "Emergency Contact Name", "Emergency Contact Number"];
+            $Data = ["true", "Child1", "None", "Parent1" , "(831)555-1754"];
+
+      foreach ($Header as $header){
+
+          echo "<td>";
+          echo $header;
+          echo "</td>";
+          }//closes foreach Loop */
+    //====================================================================
+    echo"<tr>";
+    foreach ($Data as $data){    
+        echo "<td>";
+        echo $data;
+        echo "</td>";
+    }//closes foreach Loop *///
+    echo"</tr>";
+
+        ?>
+
+        </table><!-----------End  Inserted table ------------------------------->
+                
             </div>
             
             
             <div id="body">
-                
+                <br />
+                <!--
+                <table border = '7'><!----------- Start Inserted table -----------
+    <?php
+//Row <->
+//Col ^|V
+/*This table Displays each Child's Informaton including: isPresent, child's Name,
+ *      Allergies, Emergency contact's Name, and Emergency Contact Phone Number.
+ *      This also table is formated as the following:
+ *          First for loop displays the table header.
+ *          Second for loop displays each of the child's information Row by Row.
+ */
+        $Header = ["Present", "Child Name", "Allergies", "Emergency Contact Name", "Emergency Contact Number"];
+        $Data = ["true", "Child1", "None", "Parent1" , "(831)555-1754"];
+
+    foreach ($Header as $header){
+            echo "<td>";
+            echo $header;
+            echo "</td>";
+        }//closes foreach Loop */
+//====================================================================
+echo"<tr>";
+        foreach ($Data as $data){
+            echo "<td>";
+            echo $data;
+            echo "</td>";
+        }//closes foreach Loop *///
+echo"</tr>";
+
+    ?>
+
+        </table><!-----------End  Inserted table ------------------------------->
             </div>
             
             <div id="footer">
