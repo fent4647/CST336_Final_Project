ada689587284c1ac3b1cc586e4fecc7ce755c903
saymtfmtfmtf <mfenton@csumb.edu>
2015-05-05 04:06:45 +0000

    Update Works Now

--- update.php
+++ update.php
@@ -5,15 +5,10 @@
     require('includes/childFunctions.php');    
     $dbConn = getConnection();
     
-    
-    $results = getChildInformation($_GET['childId']);
-    
-
-    if(isset($_GET["submit"])) {
+     if(isset($_GET["submit"])) {
         $sql = "UPDATE child 
-        SET firstname = $1 AND middlename = $2 AND lastname = $3
-        AND foodallergies = $4 AND allergydescription = $5 AND concerns = $6 
-        AND concernsdescription = $7 AND birthdate = $8 
+        SET (firstname, middlename, lastname, foodallergies, allergydescription, concerns, concernsdescription, birthdate)
+        = ($1, $2, $3, $4, $5, $6, $7, $8)
         WHERE childid = $9";
         pg_query_params($dbConn, $sql, array($_GET['firstName'],
                                             $_GET['middleName'],
@@ -22,13 +17,13 @@
                                             $_GET['allergiesDescription'],
                                             $_GET['concerns'],
                                             $_GET['concernsDescription'],
-                                            $_GET['birthday']));
+                                            $_GET['birthday'],
+                                            $_GET['childId']));
         
     }
-    
-/*
- * To change this template use Tools | Templates.
- */
+
+    $results = getChildInformation($_GET['childId']);
+
 ?>
 
 <!DOCTYPE HTML>
@@ -49,11 +44,11 @@
         }
         
          function checkValid() {
-            var $num = $('#pNumber').val();
-            if(!/^\(\d{3}\)\d{3}-\d{4}$/.test($num)) {
-                $('#correctFormat').html('Please enter the correct format. (555)555-5555');
+            var $num = $('#birthday').val();
+            if(!/^\d{2}-\d{2}-\d{4}$/.test($num)) {
+                $('#correctFormat').html('Please enter the correct format. 12-25-1969');
                 $('#correctFormat').css('color', '#FF0000');
-                $('#pNumber').focus();
+                $('#birthday').focus();
                 submitButton(false);
             }else {
                 $('#correctFormat').empty();
@@ -77,13 +72,14 @@
             
            
                 <form>
+                 <input type="hidden" name="childId" value="<?= $results['0'] ?>" />
                  First Name: <input type="text" name="firstName" id="firstName" value="<?= $results['1'] ?>" required/><br /> 
                  Middle Name: <input type="text" name="middleName" id="middleName" value="<?= $results['2'] ?>" /><br />
                  Last Name: <input type="text" name="lastName" id="lastName" value="<?= $results['3'] ?>"  required/><br />
-                 Food Allergies: <input type="text" name="allergies" id="allergies" placeholder="Meat" value="<?= $results['6'] ?>"  /><br />
-                 Allergy Description: <textarea name="allergiesDescription" id="allergiesDescription" value="<?= $results['7'] ?>" placeholder="Can't eat meat, what a vegan." cols="50" rows="10"></textarea><br/>
-                 Concerns: <input type="text" name="concerns" id="concerns" value="<?= $results['8'] ?>" placeholder="Hyper, ADHD"/><br />
-                 ConcernsDescription: <textarea name="concernsDescription" id="concernsDescription" value="<?= $results['9'] ?>" placeholder="He'll Do Alot of Flips." cols="50" rows="10"></textarea><br/>
+                 Food Allergies: <input type="number" name="allergies" id="allergies" placeholder="1 for YES, 0 for NO" min="0" max="1" value="<?= $results['6'] ?>"  /><br />
+                 Allergy Description: <input name="allergiesDescription" id="allergiesDescription" value="<?= $results['7'] ?>" placeholder="Can't eat meat, what a vegan." cols="50" rows="10"><br/>
+                 Concerns: <input type="number" name="concerns" id="concerns" value="<?= $results['8'] ?>" min="0" max="1" placeholder="1 for YES, 0 for NO"/><br />
+                 ConcernsDescription: <input name="concernsDescription" id="concernsDescription" value="<?= $results['9'] ?>" placeholder="He'll Do Alot of Flips." cols="50" rows="10"><br/>
                  Birthday: <input type="text" name="birthday" id="birthday" placeholder="12-25-1969" value="<?= $results['10'] ?>" /><span id="correctFormat"></span><br />
                 <input type="submit" name="submit" value="Submit" id="submitButton"/>
                 </form>
@@ -94,12 +90,6 @@
         </div>
     </body>
     <script>
-       $('#firstName').val();
-       $('#lastName').val();
-       $('#firstName').val();
-       $('#lastName').val();
-       $('#firstName').val();
-       $('#lastName').val();
        $('#birthday').change(checkValid);
     </script>
 </html>
